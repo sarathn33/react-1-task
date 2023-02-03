7945824b4c4295b24cc46a9d60aa55de4e865939
SARATH <sarath@Saraths-MacBook-Air.local>
2023-02-03 12:49:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className='container'>
+        <div className='title-card'>FREE</div>
+          <div className='title'>$0/month</div>
+          <br/>
+        <hr/>
+        <p className='content' >✅ Single User</p>
+       
+        <p className='content'>✅ 5 GB Storage</p>
+        
+        <p className='content'>✅ Unlimited Public Projects</p>
+       
+        <p className='content'>✅ Community Access</p>
+   
+        <p className='content-blocked'>❌ Unlimited Private Projects</p>
+     
+        <p className='content-blocked'>❌ Dedicated Phone Support</p>
+     
+        <p className='content-blocked'>❌ Free Sub-domain</p>
+ 
+        <p className='content-blocked'>❌ Monthly Status Reports</p>
+    
+        <button type="button" className='btn-button'>CLICK HERE </button>
+        </div>
+
+        <div className='container'>
+        <div className='title-card'>PLUS</div>
+          <div className='title'>$9/month</div>
+          <br/>
+        <hr/>
+        <p className='content' >✅ <b>5 User</b></p>
+       
+        <p className='content'>✅ 50 GB Storage</p>
+     
+        <p className='content'>✅ Unlimited Public Projects</p>
+     
+        <p className='content'>✅ Community Access</p>
+   
+        <p className='content'>✅ Unlimited Private Projects</p>
+   
+        <p className='content'>✅ Dedicated Phone Support</p>
+  
+        <p className='content'>✅ Free Sub-domain</p>
+  
+        <p className='content-blocked'>❌ Monthly Status Reports</p>
+     
+        <button type="button" className='btn-button'>CLICK HERE </button>
+        </div>
+
+        <div className='container'>
+        <div className='title-card'>PRO</div>
+          <div className='title'>$49/month</div>
+          <br/>
+        <hr/>
+        <p className='content' >✅ Unlimited User</p>
+     
+        <p className='content'>✅ 150 GB Storage</p>
+     
+        <p className='content'>✅ Unlimited Public Projects</p>
+  
+        <p className='content'>✅ Community Access</p>
+      
+        <p className='content'>✅ Unlimited Private Projects</p>
+      
+        <p className='content'>✅ Dedicated Phone Support</p>
+       
+        <p className='content'>✅ <b>Unlimited</b> Sub-domain</p>
+   
+        <p className='content'>✅ Monthly Status Reports</p>
+       
+        <button type="button" className='btn-button'>CLICK HERE </button>
+        </div>
+
+      </div>
   );
 }
 
